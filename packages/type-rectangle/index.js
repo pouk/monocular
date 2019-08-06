f376c4b02ae6a59b1d5ab3eba6ca143857ed6750
Mushegh Antaranyan <mushegh.antaranyan@gmail.com>
2019-08-06 10:39:02 +0400

    fix(type-rectangle): add methods for intersections
    - `hasIntersection`
    - `intersectionOf`

--- packages/type-rectangle/index.js
+++ packages/type-rectangle/index.js
@@ -1,1 +1,5 @@
-module.exports = require('./src/rectangle')
+const Rectangle = require('./src/rectangle')
+
+const Methods = require('./src/methods')
+
+module.exports = Object.assign(Rectangle, Methods)
