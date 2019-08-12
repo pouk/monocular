926b3f239bd442c20efe194c20a47025afe11114
Mushegh Antaranyan <mushegh.antaranyan@gmail.com>
2019-08-12 11:53:43 +0400

    chore(ui):
    - rewrite html templates to pug
    - resolve components from file or directory containing vue, js and css

--- packages/ui/src/views/Home.vue
+++ packages/ui/src/views/Home.vue
@@ -1,13 +1,12 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang="pug">
+  div.home
+    img(src="../assets/logo.png")
+    hello-world(msg="Welcome to Your Vue.js App")
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from '@/components/HelloWorld'
 
 export default {
   name: 'home',
