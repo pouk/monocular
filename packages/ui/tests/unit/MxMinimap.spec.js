25b4aceb4cf52b13e42da5abc1b27548191fc046
Mushegh Antaranyan <mushegh.antaranyan@gmail.com>
2019-08-18 17:03:19 +0400

    test(ui): remove stale MxMinimap test

--- packages/ui/tests/unit/MxMinimap.spec.js
+++ packages/ui/tests/unit/MxMinimap.spec.js
@@ -8,30 +8,3 @@ test('should render', t => {
 
   t.is(wrapper.constructor.name, 'VueWrapper')
 })
-
-test('should have proper layout', t => {
-  const propsData = {
-    originalSize: {
-      width: 1920,
-      height: 1080
-    },
-    scaleFactor: 1 / 10,
-    zoomLevel: 1
-  }
-
-  const wrapper = shallowMount(MxMinimap, {
-    propsData
-  })
-
-  const innerAttrs = wrapper
-    .find('.inner-area')
-    .attributes()
-
-  // display proportionally
-  t.true(innerAttrs.style.includes('width: 96px'))
-  t.true(innerAttrs.style.includes('height: 54px'))
-
-  // center by default
-  t.true(innerAttrs.style.includes('left: 48px'))
-  t.true(innerAttrs.style.includes('top: 27px'))
-})
