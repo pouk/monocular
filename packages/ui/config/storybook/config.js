8a873c57055b3a1ab5a9f860488cffe616b22c20
Mushegh Antaranyan <mushegh.antaranyan@gmail.com>
2019-08-20 14:16:20 +0400

    chore(ui): install Storybook for Vue componens
    Treat storybook as part of documentation
    
    Resolves #23

---
+++ packages/ui/config/storybook/config.js
@@ -0,0 +1,10 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { configure } from '@storybook/vue'
+
+const req = require.context('../../docs/stories', true, /(.+)\/index.js$/)
+
+function loadStories () {
+  req.keys().forEach(filename => req(filename))
+}
+
+configure(loadStories, module)
